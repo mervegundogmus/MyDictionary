3c6192f39ece0d4fac66620aa03b2668d2d05376
mervegundogmus <mervegundogmus@outook.com>
2021-02-24 04:52:03 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(21, "Picture");
+            myDictionary.Add(25, "Pillow");
+            myDictionary.Add(28, "Pink");
+
+            myDictionary.ToList();
+
+
+        }
+    }
+}
